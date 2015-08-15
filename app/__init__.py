b415022084f352f67d4e518255e7381b873526ca
HB6H057 <me@hb6.org>
2015-08-15 15:45:27 +0800

    impment database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
 from views import index_views, admin_views
+from models import user_models
---
+++ app/models/user_models.py
@@ -0,0 +1,25 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64))
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    @property
+    def password(self):
+        raise AttributeError('GlaDOS:"What are you doing?\
+                              (password is not a readable attribute)"')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        "GlaDOS: \"(User: %s)That was genuinely mildly impressive.\""
